5464c78e11bfd448c87f8eed9f779296b78fb41a
Frick112 <sf137412@students.davenport.k12.ia.us>
2017-01-26 16:59:43 -0600

    asdf
    asdf

---
+++ src/movieQuote/MovieQuote.java
@@ -0,0 +1,11 @@
+package movieQuote;
+
+public class MovieQuote 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Superhero landing! She’s going to do a superhero landing!");
+	}
+
+}
---
+++ src/movieQuote/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+package movieQuote;
+
+public class MovieQuoteInfo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Superhero landing! She’s going to do a superhero landing!");
+		System.out.println("Deadpool, or Ryan Renolds");
+		System.out.println("Deadpool 2016");
+	}
+}
